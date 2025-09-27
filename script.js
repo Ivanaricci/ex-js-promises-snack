d1ac1f580d3068bb48a7b344528b79a168a97d7b
Ivanaricci <ivana.ricci26@gmail.com>
2025-09-27 12:44:03 +0200

    [UPD] BONUS

--- script.js
+++ script.js
@@ -34,24 +34,24 @@ getPostTitle(1)
 // SNACK 2
 // Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
 
-function lanciaDado() {
-    return new Promise((resolve, reject) => {
-        console.log('Sto lanciando il dado');
-        setTimeout(() => {
-            const valore = Math.floor(Math.random() * 6) + 1;
-            if (valore < 0.2) {
-                reject("Dado incastrato")
-            }
-            else {
-                resolve(`${valore}`)
-            }
-        }, 3000)
-    })
-}
+// function lanciaDado() {
+//     return new Promise((resolve, reject) => {
+//         console.log('Sto lanciando il dado');
+//         setTimeout(() => {
+//             const valore = Math.floor(Math.random() * 6) + 1;
+//             if (valore < 0.2) {
+//                 reject("Dado incastrato")
+//             }
+//             else {
+//                 resolve(`${valore}`)
+//             }
+//         }, 3000)
+//     })
+// }
 
-lanciaDado()
-    .then(number => console.log(number))
-    .catch(error => console.error(error))
+// lanciaDado()
+//     .then(number => console.log(number))
+//     .catch(error => console.error(error))
 
 
 
@@ -67,3 +67,41 @@ lanciaDado()
 // 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
 // Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
 
+
+
+
+function creaLanciaDado() {
+    let ultimorisultato = null;
+
+    function lanciaDado() {
+        return new Promise((resolve, reject) => {
+            console.log('Sto lanciando il dado');
+            setTimeout(() => {
+                const valore = Math.floor(Math.random() * 6) + 1;
+                if (valore < 0.2) {
+                    reject("Dado incastrato")
+                }
+                else {
+                    if (valore === ultimorisultato) {
+                        console.log("Incredibile")
+                    }
+                    ultimorisultato = valore
+                    resolve(`${valore}`)
+                }
+
+            }, 3000)
+        })
+    }
+
+    return lanciaDado
+}
+
+const lanciaDado = creaLanciaDado()
+
+lanciaDado()
+    .then(number => console.log(number))
+    .catch(error => console.error(error))
+
+lanciaDado()
+    .then(number => console.log(number))
+    .catch(error => console.error(error))
