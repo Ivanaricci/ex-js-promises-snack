af3fa652891ca24f197b56a7010f87acdcf3110f
Ivanaricci <ivana.ricci26@gmail.com>
2025-09-27 12:19:57 +0200

    [UPD] SNACK2

--- script.js
+++ script.js
@@ -29,3 +29,41 @@ getPostTitle(1)
 // jsonTransformation.then(function (obj) {
 //     console.log(obj)
 // })
+
+
+// SNACK 2
+// Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject.
+
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        console.log('Sto lanciando il dado');
+        setTimeout(() => {
+            const valore = Math.floor(Math.random() * 6) + 1;
+            if (valore < 0.2) {
+                reject("Dado incastrato")
+            }
+            else {
+                resolve(`${valore}`)
+            }
+        }, 3000)
+    })
+}
+
+lanciaDado()
+    .then(number => console.log(number))
+    .catch(error => console.error(error))
+
+
+
+
+
+
+
+
+
+
+
+
+// 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
+// Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!".
+
